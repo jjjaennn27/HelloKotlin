6d5ed88f2fbf323fcd2c4af39f43939e5c7103e7
jea7982@naver.com <jea7982@naver.com>
2021-04-29 14:46:16 +0900

    Kotlin practice

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -12,9 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var intent : Intent
+        //var intent : Intent
         val btnJava = findViewById<Button>(R.id.btnJava)
         val btnKotlin = findViewById<Button>(R.id.btnKotlin)
+        val btnVarJava = findViewById<Button>(R.id.btnVarJava)
+        val btnVarKotlin = findViewById<Button>(R.id.btnVarKotlin)
 
         btnJava.setOnClickListener {
             startActivity(Intent(this@MainActivity, BMIJavaActivity::class.java))
@@ -22,6 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         btnKotlin.setOnClickListener {
             startActivity(Intent(this@MainActivity, BMIKotlinActivity::class.java))
+        }
+
+        btnVarJava.setOnClickListener {
+            startActivity(Intent(this@MainActivity, VariableJavaActivity::class.java))
+        }
+
+        btnVarKotlin.setOnClickListener {
+            startActivity(Intent(this@MainActivity, VariableKotlinActivity::class.java))
         }
 
     }
