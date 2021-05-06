bb532418007d8fbb3e1e6c32f5858c60342a6e8b
jea7982@naver.com <jea7982@naver.com>
2021-05-06 14:33:15 +0900

    Flow control in JAVA

---
+++ app/src/main/java/com/example/hellokotlin/FlowControlKotlinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hellokotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FlowControlKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_flowcontrol)
+    }
+}
